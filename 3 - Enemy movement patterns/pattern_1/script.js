140db49de7ecdd8b90674316fdd81ec33dbbfeb2
jumaniyozov <jumaniyozovse@gmail.com>
2022-12-18 13:47:35 +0500

    npc movement patterns

---
+++ 3 - Enemy movement patterns/pattern_1/script.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 500;
+const CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 50;
+const enemiesArray = [];
+
+// const enemyImage = new Image();
+// enemyImage.src = 'images/enemy1.png';
+let gameFrame = 0;
+
+class Enemy {
+    constructor() {
+        this.image = new Image();
+        this.image.src = '../images/enemy1.png';
+        this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    }
+
+    update() {
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+}
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+
+animate();
